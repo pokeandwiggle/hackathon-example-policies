3a9786a050585a8f16f35997664265d39f41daa4
Maximilian Li <max@pokeandwiggle.com>
2025-09-13 19:12:20 +0200

    feat: add documentation for using Lerobot directly and enhance act_config with policy_kwargs

--- src/example_policies/config_factory.py
+++ src/example_policies/config_factory.py
@@ -87,7 +87,26 @@ def create_lerobot_config(
     return cfg
 
 
-def act_config(dataset_root_dir: str, batch_size: int = 24, resume_path: str = None):
+def act_config(
+    dataset_root_dir: str,
+    batch_size: int = 24,
+    resume_path: str = None,
+    policy_kwargs: dict = None,
+):
+
+    default_kwargs = {
+        "vision_backbone": "resnet34",
+        "pretrained_backbone_weights": "ResNet34_Weights.IMAGENET1K_V1",
+        "chunk_size": 30,
+        "n_action_steps": 30,
+        "latent_dim": 64,
+        "n_decoder_layers": 7,
+    }
+
+    if policy_kwargs is not None:
+        default_kwargs.update(policy_kwargs)
+    policy_kwargs = default_kwargs
+
     cfg = create_lerobot_config(
         # Model selection: e.g., "act", "diffusion", "pi0", "smolvla"
         model_name="integrated_so3_act",
@@ -101,14 +120,7 @@ def act_config(dataset_root_dir: str, batch_size: int = 24, resume_path: str = N
         # Enable Weights & Biases for experiment tracking
         enable_wandb=True,
         resume_path=resume_path,
-        policy_kwargs={
-            "vision_backbone": "resnet34",
-            "pretrained_backbone_weights": "ResNet34_Weights.IMAGENET1K_V1",
-            "chunk_size": 30,
-            "n_action_steps": 30,
-            "latent_dim": 64,
-            "n_decoder_layers": 7,
-        },
+        policy_kwargs=policy_kwargs,
     )
     return cfg
 
@@ -117,8 +129,18 @@ def smolvla_config(
     dataset_root_dir: str,
     batch_size: int = 24,
     resume_path: str = None,
+    policy_kwargs: dict = None,
     pretrained_actions: bool = False,
 ):
+    default_kwargs = {
+        "chunk_size": 20,
+        "n_action_steps": 20,
+    }
+
+    if policy_kwargs is not None:
+        default_kwargs.update(policy_kwargs)
+    policy_kwargs = default_kwargs
+
     policy = None
     if pretrained_actions:
         policy = PreTrainedConfig.from_pretrained("lerobot/smolvla_base")
@@ -137,11 +159,58 @@ def smolvla_config(
         # Enable Weights & Biases for experiment tracking
         enable_wandb=True,
         resume_path=resume_path,
+        policy_kwargs=policy_kwargs,
+    )
+    return cfg
+
+
+def diffusion_config(
+    dataset_root_dir: str,
+    batch_size: int = 96,
+    resume_path: str = None,
+    policy_kwargs: dict = None,
+):
+    # Diffusion Policy settings:
+    n_obs_steps: int = 2
+    horizon: int = 32
+    n_action_steps: int = 16
+
+    default_kwargs = {
+        "vision_backbone": "resnet34",
+        # "pretrained_backbone_weights": "ResNet34_Weights.IMAGENET1K_V1",
+        "crop_shape": (224, 224),
+        "use_separate_rgb_encoder_per_camera": True,
+        "down_dims": (128, 256, 512, 512),
+        "kernel_size": 3,
+        "n_groups": 8,
+        "num_train_timesteps": 1000,
+        "diffusion_step_embed_dim": 512,
+        "prediction_type": "sample",
+        # "n_obs_steps": n_obs_steps,
+        "horizon": horizon,
+        "n_action_steps": n_action_steps,
+        "drop_n_last_frames": horizon - n_action_steps - n_obs_steps + 1,
+    }
+
+    if policy_kwargs is not None:
+        default_kwargs.update(policy_kwargs)
+    policy_kwargs = default_kwargs
+
+    cfg = create_lerobot_config(
+        # Model selection: e.g., "act", "diffusion", "pi0", "smolvla"
+        model_name="integrated_so3_diffusion",
+        # Path to the LeRobot dataset directory
+        dataset_root_dir=dataset_root_dir,
+        # Training hyperparameters
+        batch_size=batch_size,
+        lr=1e-4,
+        steps=400_000,
+        save_freq=8_000,
+        # Enable Weights & Biases for experiment tracking
+        enable_wandb=True,
+        resume_path=resume_path,
         # Additional Policy Keywords
-        policy_kwargs={
-            "chunk_size": 20,
-            "n_action_steps": 20,
-        },
+        policy_kwargs=policy_kwargs,
     )
     return cfg
 
@@ -176,46 +245,4 @@ def pi0_config(
         },
     )
 
-    return cfg
-
-
-def diffusion_config(
-    dataset_root_dir: str, batch_size: int = 96, resume_path: str = None
-):
-    # Diffusion Policy settings:
-    n_obs_steps: int = 2
-    horizon: int = 32
-    n_action_steps: int = 16
-
-    cfg = create_lerobot_config(
-        # Model selection: e.g., "act", "diffusion", "pi0", "smolvla"
-        model_name="integrated_so3_diffusion",
-        # Path to the LeRobot dataset directory
-        dataset_root_dir=dataset_root_dir,
-        # Training hyperparameters
-        batch_size=batch_size,
-        lr=1e-4,
-        steps=400_000,
-        save_freq=8_000,
-        # Enable Weights & Biases for experiment tracking
-        enable_wandb=True,
-        resume_path=resume_path,
-        # Additional Policy Keywords
-        policy_kwargs={
-            "vision_backbone": "resnet34",
-            # "pretrained_backbone_weights": "ResNet34_Weights.IMAGENET1K_V1",
-            "crop_shape": (224, 224),
-            "use_separate_rgb_encoder_per_camera": True,
-            "down_dims": (128, 256, 512, 512),
-            "kernel_size": 3,
-            "n_groups": 8,
-            "num_train_timesteps": 1000,
-            "diffusion_step_embed_dim": 512,
-            "prediction_type": "sample",
-            # "n_obs_steps": n_obs_steps,
-            "horizon": horizon,
-            "n_action_steps": n_action_steps,
-            "drop_n_last_frames": horizon - n_action_steps - n_obs_steps + 1,
-        },
-    )
     return cfg
