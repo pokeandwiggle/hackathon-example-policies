b8bbf7307d316a328662a14a27cb985b07120d9e
JC <jccalvojackson@gmail.com>
2025-09-17 20:11:22 +0200

    chore: to lerobot

--- scripts/01_create_dataset.py
+++ scripts/01_create_dataset.py
@@ -34,14 +34,14 @@ from example_policies.data_ops.config.pipeline_config import ActionLevel, Pipeli
 
 # --- Paths ---
 # TODO: Set the input directory containing your .mcap files.
-RAW_DATA_DIR = pathlib.Path("data/wandb/filtered_artifacts")
+RAW_DATA_DIR = pathlib.Path("data/step_1_flip")
 
 # TODO: Set your desired output directory name.
 OUTPUT_DIR = pathlib.Path("data/lerobot/filtered_dataset")
 
 # --- Configuration ---
 # TODO: A descriptive label for the task, used for VLA-style text conditioning.
-TASK_LABEL = "step_1"
+TASK_LABEL = "step_1_flip"
 
 cfg = PipelineConfig(
     include_joint_positions=True,
