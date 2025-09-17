2de981f3ba66e4ec1ec56848dc8a71f7a6e30b6f
JC <jccalvojackson@gmail.com>
2025-09-17 06:15:00 +0200

    chore: simpler train script

--- scripts/02_train_model_simple.py
+++ scripts/02_train_model_simple.py
@@ -1,51 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # 🚀 Model Training
-#
-# This notebook guides you through the process of training a policy on your converted dataset.
-#
-# The process is broken down into a few simple steps:
-# 1.  **Setup**: Apply necessary patches to the `lerobot` library.
-# 2.  **Dataset**: Specify the path to your training data.
-# 3.  **Configuration**: Select a model architecture and its hyperparameters.
-# 4.  **Training**: Launch the training process.
-
-# ---
-# ### 1. Setup
-#
-# First, apply our custom patches to the `lerobot` library. This only needs to be done once per session.
-
-# In[1]:
-
+from lerobot.datasets.transforms import ImageTransformsConfig
 
 from example_policies import lerobot_patches
+from example_policies.config_factory import act_config, diffusion_config, smolvla_config
 
 lerobot_patches.apply_patches()
-
-# ---
-# ### 2. Select Dataset
-#
-# > **Action Required:** Update `DATA_DIR` to point to the dataset you created in the previous notebook.
-
-# In[3]:
-
-
-import pathlib
-
-# TODO: Set the path to your converted dataset directory.
-DATA_DIR = pathlib.Path("../data/my_awesome_dataset")
-
-# ---
-# ### 3. Select Model Configuration
-#
-# We provide several pre-made configurations as a starting point. Uncomment the model you wish to use. You can also adjust parameters like `batch_size` as needed.
-
-# In[ ]:
-
-
-# Select one of the following configurations
-from example_policies.config_factory import act_config, diffusion_config, smolvla_config
 
 repo_id = "jccj/mh2_step_1"
 cfg = act_config(
