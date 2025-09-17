03b4b91efa60d7bad7022a65a1bec4e7d1687c67
JC <jccalvojackson@gmail.com>
2025-09-18 00:23:56 +0200

    chore: push lerobot joint

--- scripts/push_dataset.py
+++ scripts/push_dataset.py
@@ -5,7 +5,7 @@ from example_policies import lerobot_patches
 lerobot_patches.apply_patches()
 
 
-repo_id = "jccj/mh2_step_1_flip"
+repo_id = "jccj/mh2_step_1_flip_joint"
 dataset = LeRobotDataset(
     repo_id=repo_id, root="data/lerobot/filtered_dataset_joint"
 ).push_to_hub()
