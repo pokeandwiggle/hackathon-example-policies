083d1c3baa6dc82748bda155ea718726851e7ea8
JC <jccalvojackson@gmail.com>
2025-09-18 23:53:26 +0200

    chore: finetune

---
+++ scripts/finetune.py
@@ -0,0 +1,25 @@
+from lerobot.datasets.transforms import ImageTransformsConfig
+
+from example_policies import lerobot_patches
+from example_policies.config_factory import (
+    act_config,
+    diffusion_config,
+    original_act_config,
+    smolvla_config,
+)
+from example_policies.train import train
+
+lerobot_patches.apply_patches()
+
+# repo_id = "jccj/mh2_step_1_and_2"
+dataset_root_dir = "data/lerobot/step_1_and_2"
+cfg = original_act_config(
+    # repo_id=repo_id,
+    dataset_root_dir=dataset_root_dir,
+    batch_size=16,
+)
+
+cfg.policy.pretrained_path = "outputs/checkpoints_vastai/080000"
+
+
+train(cfg)
