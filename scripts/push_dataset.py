2ccdd45083f0b5f591e4706ad0f20d4fbb9d03bd
JC <jccalvojackson@gmail.com>
2025-09-18 17:45:50 +0200

    chore: push lerobot step 1 recovery pre clean

--- scripts/push_dataset.py
+++ scripts/push_dataset.py
@@ -5,7 +5,7 @@ from example_policies import lerobot_patches
 lerobot_patches.apply_patches()
 
 
-repo_id = "jccj/mh2_step_2_pre_clean"
+repo_id = "jccj/mh2_step_1_recovery_pre_clean"
 dataset = LeRobotDataset(
-    repo_id=repo_id, root="data/lerobot/step_2_pre_clean"
+    repo_id=repo_id, root="data/lerobot/step_1_recovery_pre_clean"
 ).push_to_hub()
