48a1e03289a235bef9355ca7f4155f217d91476b
JC <jccalvojackson@gmail.com>
2025-09-19 00:05:42 +0200

    chore: push step 1 with recovery

--- scripts/push_dataset.py
+++ scripts/push_dataset.py
@@ -5,8 +5,8 @@ from example_policies import lerobot_patches
 lerobot_patches.apply_patches()
 
 
-repo_id = "jccj/mh2_step_1_and_2"
+repo_id = "jccj/mh2_step_1_with_recovery"
 dataset = LeRobotDataset(
     repo_id=repo_id,
-    root="data/lerobot/step_1_and_2",
+    root="data/lerobot/step_1_with_recovery",
 ).push_to_hub()
