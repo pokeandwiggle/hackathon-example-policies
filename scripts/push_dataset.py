29dccb26b8e40380a20793ee1959247fd95f05b8
JC <jccalvojackson@gmail.com>
2025-09-18 23:54:17 +0200

    chore: push step 1 merged with 2

--- scripts/push_dataset.py
+++ scripts/push_dataset.py
@@ -5,8 +5,8 @@ from example_policies import lerobot_patches
 lerobot_patches.apply_patches()
 
 
-repo_id = "jccj/mh2_step_1_with_recovery"
+repo_id = "jccj/mh2_step_1_and_2"
 dataset = LeRobotDataset(
     repo_id=repo_id,
-    root="data/lerobot/step_1_with_recovery",
+    root="data/lerobot/step_1_and_2",
 ).push_to_hub()
