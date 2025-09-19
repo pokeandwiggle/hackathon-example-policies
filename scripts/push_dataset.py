f010c37838a039d4fa6a86dfd5d78c601975f14d
JC <jccalvojackson@gmail.com>
2025-09-19 13:43:11 +0200

    chore; try tcp

--- scripts/push_dataset.py
+++ scripts/push_dataset.py
@@ -5,8 +5,8 @@ from example_policies import lerobot_patches
 lerobot_patches.apply_patches()
 
 
-repo_id = "jccj/mh2_step_1_2_and_3"
+repo_id = "jccj/mh2_step_2_tcp"
 dataset = LeRobotDataset(
     repo_id=repo_id,
-    root="data/lerobot/step_1_2_and_3",
+    root="data/lerobot/step_2_tcp",
 ).push_to_hub()
