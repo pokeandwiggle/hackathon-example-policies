1dffa9cb9ae2c2dda3abfbb5f15d8681822b2163
JC <jccalvojackson@gmail.com>
2025-09-19 08:06:31 +0200

    chore: push step 1,2, and 3 ds

--- scripts/push_dataset.py
+++ scripts/push_dataset.py
@@ -5,8 +5,8 @@ from example_policies import lerobot_patches
 lerobot_patches.apply_patches()
 
 
-repo_id = "jccj/mh2_step_1_and_2"
+repo_id = "jccj/mh2_step_1_2_and_3"
 dataset = LeRobotDataset(
     repo_id=repo_id,
-    root="data/lerobot/step_1_and_2",
+    root="data/lerobot/step_1_2_and_3",
 ).push_to_hub()
